16a520ab73e8e8bea9e3fbb4455498a964154753
Raiff Smith Said <raiff.said@gmail.com>
2020-12-08 11:56:05 -0300

    Feat: Updated about.njk page using Nunjucks

--- iniciando_backend/data.js
+++ iniciando_backend/data.js
@@ -6,33 +6,33 @@ module.exports = [
         price: "Free"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "57 min",
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "36 min",
         price: "Free"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "57 min",
+        id: "rAzHvYnQ8DY",
+        title: "Array: High Order Functions | Mayk Brito",
+        duration: "54 min",
         price: "Free"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "57 min",
+        id: "ghTrp1x_1As",
+        title: "O que é API? REST e RESTful? | Mayk Brito",
+        duration: "33 min",
         price: "Free"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "57 min",
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável this no Javascript | Mayk Brito",
+        duration: "48 min",
         price: "Free"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "57 min",
+        id: "2alg7MQ6_sI",
+        title: "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
+        duration: "33 min",
         price: "Free"
     }
 ]
--- iniciando_backend/server.js
+++ iniciando_backend/server.js
@@ -9,15 +9,28 @@ server.use(express.static('public'));
 server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 });
 
 server.get('/', (req, res) => {
-    return res.render('about');
+    const data = {
+        avatar_url: 'https://avatars0.githubusercontent.com/u/28994297?s=460&u=8257d240f1ad1a105abe224ff1e131a9f0f872e6&v=4',
+        name: 'Raiff Said',
+        role: 'Desenvolvedor Node.js',
+        description: 'Focado em aprender Node.js, ReactJS e React Native',
+        links: [
+            { name: 'Github', url: 'https://github.com/raiffsaid' },
+            { name: 'LinkedIn', url: 'https://www.linkedin.com/in/raiffsaid' }
+        ]
+    };
+
+
+    return res.render('about', { about: data });
 });
 
 server.get('/portfolio', (req, res) => {
-    return res.render('portfolio');
+    return res.render('portfolio', { items: videos });
 });
 
 server.listen(5000, () => {
