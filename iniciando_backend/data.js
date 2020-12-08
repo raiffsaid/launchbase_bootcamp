fd930f7a1c3ce5092980147bd2d6ef0b65365f03
Raiff Smith Said <raiff.said@gmail.com>
2020-12-08 12:10:45 -0300

    Feat: Added featured videos with dynamic CSS classes

--- iniciando_backend/data.js
+++ iniciando_backend/data.js
@@ -3,7 +3,8 @@ module.exports = [
         id: "GykTLqODQuU",
         title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
         duration: "57 min",
-        price: "Free"
+        price: "Free",
+        featured: true
     },
     {
         id: "HN1UjzRSdBk",
@@ -21,7 +22,8 @@ module.exports = [
         id: "ghTrp1x_1As",
         title: "O que é API? REST e RESTful? | Mayk Brito",
         duration: "33 min",
-        price: "Free"
+        price: "Free",
+        featured: true
     },
     {
         id: "GSqR2i-Pq6o",
@@ -33,6 +35,7 @@ module.exports = [
         id: "2alg7MQ6_sI",
         title: "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
         duration: "33 min",
-        price: "Free"
+        price: "Free",
+        featured: true
     }
 ]
